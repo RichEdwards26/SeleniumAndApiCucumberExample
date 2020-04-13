aca7ea95ef5bd9fc3499be85f7baef0b8033d5cd
Rich Edwards <36890467+RichEdwards26@users.noreply.github.com>
2020-04-13 14:13:41 +0100

    API for create account done

--- src/test/java/DataModels/ArticleParentData.java
+++ src/test/java/DataModels/ArticleParentData.java
@@ -1,4 +1,24 @@
 package DataModels;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class ArticleParentData {
+    @SerializedName("article")
+    @Expose
+    private ArticleData article;
+
+    public ArticleData getArticle() {
+        return article;
+    }
+
+    public void setArticle(ArticleData article) {
+        this.article = article;
+    }
+
+    public ArticleParentData()
+    {
+        article = new ArticleData();
+    }
+
 }
